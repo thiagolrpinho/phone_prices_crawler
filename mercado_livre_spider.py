21e84256d640cdd964f3c59788fcc4355bf14431
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-06-19 15:19:25 -0300

    :recycle: refactor: commented steps and corrected url symbol mismatching in merge

--- mercado_livre_spider.py
+++ mercado_livre_spider.py
@@ -20,6 +20,7 @@ from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 
 class MercadoLivreSpider(scrapy.Spider):
+    ROBOTSTXT_OBEY = True
     name = 'mercado_livre'
     novo_filter_xpath = '/html/body/main/div[2]/div/aside/section[2]/dl[@id="id_ITEM_CONDITION"]/dd[1]/a/@href'
     products_articles_criteria = '@class="results-item highlighted article stack product "'
@@ -31,7 +32,7 @@ class MercadoLivreSpider(scrapy.Spider):
     
     def parse(self, response):
         url = response.request.url
-        device_item_name = url[url.rfind('/')+1:]
+        device_item_url = parse.unquote(url)
 
         if response and response.body:
             response_content_str = response.body
@@ -58,45 +59,16 @@ class MercadoLivreSpider(scrapy.Spider):
                     device_price = 0
                     raw_price_numbers_list = device_raw_price.split('.')
                     for i, number in enumerate(raw_price_numbers_list):
-                        device_price += int(number)*10**((len(raw_price_numbers_list)-i-1)*3)
+                        device_price += int(number)*10**(
+                            (len(raw_price_numbers_list)-i-1)*3)
                     device_price = int(device_price)
                 else:
                     device_price = int(device_raw_price)
-                found_elements.append(
-                    [device_item_name, url, [], device_name, device_price])
+                FOUND_ELEMENTS.append(
+                    [device_item_url, device_name, device_price])
         else:
-            found_elements.append(
-                [device_item_name, url, [], [], []])
+            FOUND_ELEMENTS.append(
+                [device_item_url, [], []])
 
 
-found_elements = []
-parent_folder = Path(os.path.dirname(__file__))
-print("rugoso ", parent_folder)
-
-RELATIVE_PATH_TO_DATA_FOLDER = os.path.join(
-    parent_folder, "data/")
-filename = 'devices_using_ribonapp_data'
-filename_with_path = RELATIVE_PATH_TO_DATA_FOLDER + filename + '.csv'
-devices_df = pd.read_csv(
-    filename_with_path, skiprows=1, names=['model_name', 'old_price_range'])
-
-devices_df.drop_duplicates(['model_name'], inplace=True)
-devices_df.dropna(axis=0, inplace=True)
-
-informative_names_mask = devices_df['model_name'].str.len() > 1
-devices_df = devices_df[informative_names_mask]
-
-url_base = "https://celulares.mercadolivre.com.br/"
-item_list = devices_df['model_name'].to_list()
-
-process = CrawlerProcess()
-process.crawl(
-    MercadoLivreSpider, start_urls=[url_base + parse.quote(item_name) for item_name in item_list])
-
-process.start()
-
-extracted_data_df = pd.DataFrame(
-    data=found_elements, columns=['item_original_name', 'url', 'improved_url', 'device_name', 'device_price'])
-extracted_filename = 'scrapy_found_devices'
-extracted_filename_with_path = RELATIVE_PATH_TO_DATA_FOLDER + extracted_filename + '.csv'
-extracted_data_df.to_csv(extracted_filename_with_path, index=False)
+FOUND_ELEMENTS = []
